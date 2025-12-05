827464093fc0ab9b91dfe09b478420baf62803b4
Rullabcde <choirulrasyid09@gmail.com>
2025-12-05 15:00:45 +0700

    Revert "Fix container list options in metrics collection: Updated the import statement for container list options in collector.go to use the correct type from the Docker API."
    This reverts commit c5d5c21ba21dc80bc12050cdb33a2e4823598d9e.

--- collector/collector.go
+++ collector/collector.go
@@ -10,6 +10,7 @@ import (
 	"observex-agent/models"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 	"github.com/shirou/gopsutil/v3/cpu"
 	"github.com/shirou/gopsutil/v3/disk"
@@ -133,7 +134,7 @@ func collectDockerMetrics() (*models.DockerInfo, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{All: true})
+	containers, err := cli.ContainerList(ctx, container.ListOptions{All: true})
 	if err != nil {
 		return nil, err
 	}
